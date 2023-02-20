2c0ebc6820ef8d38740dceae030ab24ac9ee176b
alexxk2 <alexx.koda@gmail.com>
2023-02-20 18:09:50 +0300

    Commit 20.02.2023 -сделал 3 блок домашки

--- app/src/main/java/com/example/layoutmake/SearchActivity.kt
+++ app/src/main/java/com/example/layoutmake/SearchActivity.kt
@@ -1,11 +1,59 @@
 package com.example.layoutmake
+
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.layoutmake.databinding.ActivitySearchBinding
 
 
 class SearchActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        binding = ActivitySearchBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val textWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!p0.isNullOrEmpty()) binding.removeInputButton.visibility = View.VISIBLE
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.isNullOrEmpty()) binding.removeInputButton.visibility = View.INVISIBLE
+            }
+        }
+
+        with(binding) {
+            searchEditText.addTextChangedListener(textWatcher)
+            removeInputButton.setOnClickListener {
+                clearInput()
+                hideKeyboard(it)
+            }
+            arrowBackButton.setOnClickListener {
+                finish()
+            }
+        }
     }
+
+    private fun clearInput() {
+        binding.searchEditText.setText("")
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
